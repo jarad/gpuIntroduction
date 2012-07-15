b2782fbdcc6348b0bc2a5c26a3807f6af000ff78
wlandau <landau@iastate.edu>
2012-07-15 15:33:51 -0500

    worked on the intro to CUDA C lecture a bit.

--- CUDA_C_sandbox/simple/simple2.cu
+++ CUDA_C_sandbox/simple/simple2.cu
@@ -1,26 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-__global__ void colonel(int *dev_b, int *dev_a){
-  *dev_b = *dev_a + 1;
+__global__ void colonel(int *dev_a){
+  *dev_a = *dev_a + 1;
 }
 
 int main(){
 
   // Declare variables and allocate memory on the GPU.
-  int a[1], b[1], *dev_a, *dev_b;
+  int a[1], *dev_a;
   cudaMalloc((void**) &dev_a, sizeof(int));
-  cudaMalloc((void**) &dev_b, sizeof(int));
 
-  // Intitialize argument a, executed kernel, and store result in b.
+  // Intitialize argument a, executed kernel, and store result back in a.
   a[0] = 1; // REMEMBER: INDEXING IN C STARTS FROM 0.
-  cudaMemcpy(dev_a, a, sizeof(int), cudaMemcpyHostToDevice); // AFTER THIS LINE, *dev_a CANNOT BE CHANGED!
-  colonel<<<1,1>>>(dev_b, dev_a);
-  cudaMemcpy(b, dev_b, sizeof(int), cudaMemcpyDeviceToHost);
+  cudaMemcpy(dev_a, a, sizeof(int), cudaMemcpyHostToDevice); 
+  colonel<<<1,1>>>(dev_a);
+  cudaMemcpy(a, dev_a, sizeof(int), cudaMemcpyDeviceToHost);
 
   // Print result and free dynamically allocated memory.
-  printf("b[0] = %d\n", b[0]); // REMEMBER: INDEXING IN C STARTS FROM 0.
+  printf("a[0] = %d\n", a[0]); // REMEMBER: INDEXING IN C STARTS FROM 0.
   cudaFree(dev_a);
-  cudaFree(dev_b);
 
 }
