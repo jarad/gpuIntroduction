26dab32a4009a3ec9d34a65cb0a01ba599f0d4d6
Will Landau <landau@iastate.edu>
2012-09-03 00:36:39 -0500

    added lock.h

---
+++ CUDA_C_sandbox/common/lock.h
@@ -0,0 +1,24 @@
+#include "../common/book.h"
+
+struct Lock { 
+  int *mutex;
+  Lock( void ) {
+    int state = 0;
+    HANDLE_ERROR( cudaMalloc( (void**)& mutex, 
+                              sizeof(int) ) );
+    HANDLE_ERROR( cudaMemcpy( mutex, &state, sizeof(int), 
+                              cudaMemcpyHostToDevice ) );
+  }
+  
+  ~Lock( void ) { 
+    cudaFree( mutex );
+  }
+
+  __device__ void lock( void ) {
+    while( atomicCAS( mutex, 0, 1 ) != 0 );
+  }
+
+  __device__ void unlock( void ) { 
+    atomicExch( mutex, 0 );
+  } 
+};
