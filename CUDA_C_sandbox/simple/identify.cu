6c83de9b269c3363ce4410c3c7c3a8ce778e409c
wlandau <landau@iastate.edu>
2012-09-21 11:22:16 -0500

    .

--- CUDA_C_sandbox/simple/identify.cu
+++ CUDA_C_sandbox/simple/identify.cu
@@ -1,27 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-__global__ void isExecuted(int *dev_a, int blockid, int threadid){
-  
+__global__ void isExecuted(int *a_d, int blockid, int threadid){
   if(blockIdx.x == blockid && threadIdx.x == threadid)
-    *dev_a = 1;
-
+    *a_d = 1;
 }
 
 int main(){
 
-  // Declare variables and allocate memory on the GPU.
-  int init[1], a[1], *dev_a;
-  cudaMalloc((void**) &dev_a, sizeof(int));
+  int a = 0, *a_d;
+  
+  cudaMalloc((void**) &a_d, sizeof(int));
+  cudaMemcpy(a_d, &a, sizeof(int), cudaMemcpyHostToDevice);
 
-  // Initialize dev_a, execute kernel, and copy the result to CPU memory.
-  init[1] = 0;
-  cudaMemcpy(dev_a, init, sizeof(int), cudaMemcpyHostToDevice);
-  isExecuted<<<100,100>>>(dev_a, 2, 4); // NOTE: INDEXING OF THREADS AND BLOCKS STARTS FROM 0.
-  cudaMemcpy(a, dev_a, sizeof(int), cudaMemcpyDeviceToHost);
+  isExecuted<<<100,100>>>(a_d, 2, 4); 
+  
+  cudaMemcpy(&a, a_d, sizeof(int), cudaMemcpyDeviceToHost);
 
-  // Print result and free dynamically allocated memory.
-  printf("a[0] = %d\n", a[0]); // REMEMBER: INDEXING IN C STARTS FROM 0.
-  cudaFree(dev_a);
+  printf("a = %d\n", a);
+  cudaFree(a_d);
 
 }
