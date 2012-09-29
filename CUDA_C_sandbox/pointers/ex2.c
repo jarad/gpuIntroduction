ac87b6804c1da683ef38ed1eac045887a0c0311f
Will Landau <landau@iastate.edu>
2012-09-29 00:04:12 -0500

    added pointers example code

---
+++ CUDA_C_sandbox/pointers/ex2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+  int a = 0;
+  int *pa;
+  
+  *pa = a;
+  *pa = *pa + 1;
+  
+  printf("a = %d\n", a);
+  printf("*pa = %d\n", *pa);
+  printf("pa = %d\n", pa);
+}
