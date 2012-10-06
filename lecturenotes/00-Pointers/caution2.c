39aed225892518d1e784d2daba76cd836dfe872f
Will Landau <landau@iastate.edu>
2012-10-05 21:57:41 -0500

    .

--- lecturenotes/00-Pointers/caution2.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(){
-  int i = 0, *a;
-  *a = i;
-  printf("*a = %d\n", *a);
-  
-  *(a + 10000) = 1;
-}
--- lecturenotes/00-Pointers/ex1.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main(){
-  int a = 0;
-  
-  printf("a = %d\n", a);
-  printf("&a = %d\n", &a);
-}
