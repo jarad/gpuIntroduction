93d859f68888127a05ada3729872bed5424d3b30
wlandau <landau@iastate.edu>
2012-12-19 21:14:14 -0600

    using device vectors

--- CUDA_C_sandbox/Thrust/scans/scans.cu
+++ CUDA_C_sandbox/Thrust/scans/scans.cu
@@ -1,9 +1,17 @@
 #include <thrust/scan.h>
+#include <thrust/device_vector.h>
 #include <iostream>
 
 int main(){
-  int data[6] = {1, 0, 2, 2, 1, 3};
-  thrust::inclusive_scan(data, data + 6, data); // in-place scan
+  thrust::device_vector<int> data(6, 0);
+  data[0] = 1;
+  data[1] = 0;
+  data[2] = 2;
+  data[3] = 2;
+  data[4] = 1;
+  data[5] = 3;
+
+  thrust::inclusive_scan(data.begin(), data.end(), data.begin()); // in-place scan
   // data is now {1, 1, 3, 5, 6, 9}
 
  /* data[0] = data[0]
@@ -12,16 +20,21 @@ int main(){
   * ...
   * data[5] = data[0] + data[1] + ... + data[5]
   */ 
-  
-  int data2[6] = {1, 0, 2, 2, 1, 3};
-  thrust::exclusive_scan(data2, data2 + 6, data2); // in-place scan
 
-  // data2 is now {0, 1, 1, 3, 5, 6}
+  data[0] = 1;
+  data[1] = 0;
+  data[2] = 2;
+  data[3] = 2;
+  data[4] = 1;
+  data[5] = 3;
+  thrust::exclusive_scan(data.begin(), data.end(), data.end()); // in-place scan
+
+  // data is now {0, 1, 1, 3, 5, 6}
   
- /* data2[0] = 0
-  * data2[1] = data2[0]
-  * data2[2] = data2[0] + data2[1]
+ /* data[0] = 0
+  * data[1] = data[0]
+  * data[2] = data[0] + data[1]
   * ...
-  * data2[5] = data2[0] + data2[1] + ... + data[4]
+  * data[5] = data[0] + data[1] + ... + data[4]
   */ 
 }
